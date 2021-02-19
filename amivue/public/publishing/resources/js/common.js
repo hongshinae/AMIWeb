9372a3fc7a5792b182b3d0b1bc506f74d1046103
LeePyeongBok <pblee@cnuglobal.com>
2021-02-19 16:13:20 +0900

    20210209 퍼블리싱 작업분 추가
    Signed-off-by: LeePyeongBok <pblee@cnuglobal.com>

---
+++ amivue/public/publishing/resources/js/common.js
@@ -0,0 +1,22 @@
+{
+  data() {
+    return {
+      name: 'BootstrapVue',
+      show: true
+    }
+  },
+  watch: {
+    show(newVal) {
+      console.log('Alert is now ' + (newVal ? 'visible' : 'hidden'))
+    }
+  },
+  methods: {
+    toggle() {
+      console.log('Toggle button clicked')
+      this.show = !this.show
+    },
+    dismissed() {
+      console.log('Alert dismissed')
+    }
+  }
+}
