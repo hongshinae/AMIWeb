94090fc2d0d767c4d087494e36613410a5c89c3d
hong <tlsdoakstp@gmail.com>
2021-02-28 00:38:22 +0900

    메인페이지 구현 (공통포함) #1
    bill페이지에 login작업
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- amivue/src/pages/Menu/Bill/Bill.vue
+++ amivue/src/pages/Menu/Bill/Bill.vue
@@ -1,5 +1,63 @@
 <template>
-	<div class="about">
-		<h1>This is an about page</h1>
+	<div class="loginBg">
+		<div class="loginWrap">
+			<div class="logo"></div>
+			<div class="lnputWrap">
+				<b-input v-model="text" placeholder="id"></b-input>
+				<b-input v-model="password" placeholder="password"></b-input>
+				<b-button block variant="primary">로그인</b-button>
+			</div>
+			<ul>
+				<router-link tag="li" to="">아이디나 비밀번호가 기억나지 않으세요?</router-link>
+				<router-link tag="li" to=""><a>아이디 비밀번호 찾기</a></router-link>
+			</ul>
+			<div class="lnputWrap">
+				<b-button block variant="outline-primary">회원 가입</b-button>
+			</div>
+			<div class="copyright">COPYRIGHT ⓒ CUN GLOBAL Corp.</div>
+		</div>
 	</div>
 </template>
+
+<script></script>
+
+<style lang="scss">
+$color-bg: #ecf2f6;
+.loginBg {
+	width: 100%;
+	height: 100%;
+	background: $color-bg;
+}
+.loginWrap {
+	width: 500px;
+	height: 520px;
+	margin: 0 auto;
+	color: #6c757d;
+	.logo {
+		width: 362px;
+		height: 53px;
+		background: url("../../../../src/assets/images/logo_login.png") 0 0 no-repeat;
+		margin: 10px auto 23px auto;
+	}
+	.lnputWrap {
+		input {
+			margin-bottom: 10px;
+		}
+	}
+	ul {
+		width: 500px;
+		height: 30px;
+		margin-top: 20px;
+		li {
+			float: left;
+		}
+		li:last-child {
+			float: right;
+		}
+	}
+	.copyright {
+		margin: 20px;
+		text-align: center;
+	}
+}
+</style>
