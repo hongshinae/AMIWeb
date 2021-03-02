da994be8047a99bc7e07e2d249b46b67b9caed72
hong <tlsdoakstp@gmail.com>
2021-03-02 15:10:36 +0900

    메인페이지 구현 (공통포함) #1
    header.vue
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- amivue/src/components/layout/Header.vue
+++ amivue/src/components/layout/Header.vue
@@ -1,17 +1,20 @@
 <template>
-	<div>
-		<nav class="navbar">
-			<a class="sidebar-toggle">
-				<i class="menu"></i>
-			</a>
-			<ul class="infowrap">
-				<li class="error on"><i></i></li>
-				<li class="aram"><i></i></li>
-				<li class="user">씨앤유</li>
-				<li class="uerName">관리자</li>
-				<li class="logout"><a href="#">로그아웃</a></li>
-			</ul>
-		</nav>
+	<div class="navWrap">
+		<b-row>
+			<b-navbar variant="faded">
+				<b-navbar-brand href="#"></b-navbar-brand>
+				<a class="sidebar-toggle">
+					<b-button v-b-toggle.sidebar-1 class="menu"></b-button>
+				</a>
+			</b-navbar>
+			<b-nav class="infowrap ml-auto">
+				<b-nav-item class="error"><i></i></b-nav-item>
+				<b-nav-item class="aram"><i></i></b-nav-item>
+				<b-nav-item class="user" disabled>씨앤유</b-nav-item>
+				<b-nav-item class="uerName">관리자</b-nav-item>
+				<b-nav-item class="logout" href="#">로그아웃</b-nav-item>
+			</b-nav>
+		</b-row>
 	</div>
 </template>
 
