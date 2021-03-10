22006ff2d60dad8a53034fe3137d91ce4097a4b9
LeePyeongBok <pblee@cnuglobal.com>
2021-03-10 18:03:11 +0900

    메인페이지 구현 (공통포함) #1
    1. 메인 레이아웃 영역 임시 커밋
     - sidebar 플러그인 추가
    
    Signed-off-by: LeePyeongBok <pblee@cnuglobal.com>

--- amivue/src/pages/Layout/MainLayout.vue
+++ amivue/src/pages/Layout/MainLayout.vue
@@ -1,6 +1,14 @@
 <template>
 	<div id="app" class="wrapper status-board">
-		<Menu />
+		<!-- <Menu /> -->
+		<sidebar-menu
+			:menu="menu"
+			:collapsed="collapsed"
+			:theme="selectedTheme"
+			:show-one-child="true"
+			@toggle-collapse="onToggleCollapse"
+			@item-click="onItemClick"
+		/>
 		<div class="main">
 			<Header />
 			<Dashboard-Content />
@@ -9,16 +17,180 @@
 </template>
 
 <script>
-import Menu from "@/components/layout/Menu";
+import Vue from "vue";
+import VueSideBar from "@/components/plugin/VueSideBar";
+// import Menu from "@/components/layout/Menu";
 import Header from "@/components/layout/Header";
 import DashboardContent from "@/components/layout/DashboardContent";
 
+Vue.use(VueSideBar);
+
+const separator = {
+	template: `<hr style="border-color: rgba(0, 0, 0, 0.1); margin: 20px;">`
+};
 export default {
 	name: "App",
 	components: {
 		Header,
-		Menu,
+		// Menu,
 		DashboardContent
+	},
+	mounted() {
+		this.onResize();
+		window.addEventListener("resize", this.onResize);
+	},
+	methods: {
+		onToggleCollapse(collapsed) {
+			console.log(collapsed);
+			this.collapsed = collapsed;
+		},
+		onItemClick(/*event, item, node*/) {
+			console.log("onItemClick");
+			// console.log(event)
+			// console.log(item)
+			// console.log(node)
+		},
+		onResize() {
+			if (window.innerWidth <= 767) {
+				this.isOnMobile = true;
+				this.collapsed = true;
+			} else {
+				this.isOnMobile = false;
+				this.collapsed = false;
+			}
+		}
+	},
+	data() {
+		return {
+			collapsed: false,
+			themes: [
+				{
+					name: "Default theme",
+					input: ""
+				},
+				{
+					name: "White theme",
+					input: "white-theme"
+				}
+			],
+			selectedTheme: "white-theme",
+			isOnMobile: false,
+			menu: [
+				{
+					header: true,
+					title: "Getting Started",
+					hiddenOnCollapse: true
+				},
+				{
+					href: "/",
+					title: "Installation",
+					icon: "fa fa-download"
+				},
+				{
+					href: "/basic-usage",
+					title: "Basic Usage",
+					icon: "fa fa-code"
+				},
+				{
+					header: true,
+					title: "Usage",
+					hiddenOnCollapse: true
+				},
+				{
+					href: "/props",
+					title: "Props",
+					icon: "fa fa-cogs"
+				},
+				{
+					href: "/events",
+					title: "Events",
+					icon: "fa fa-bell"
+				},
+				{
+					href: "/styling",
+					title: "Styling",
+					icon: "fa fa-palette"
+				},
+				{
+					component: separator
+				},
+				{
+					header: true,
+					title: "Example",
+					hiddenOnCollapse: true
+				},
+				{
+					href: "/disabled",
+					title: "Disabled page",
+					icon: "fa fa-lock",
+					disabled: true
+				},
+				{
+					title: "Badge",
+					icon: "fa fa-cog",
+					badge: {
+						text: "new",
+						class: "vsm--badge_default"
+					}
+				},
+				{
+					href: "/page",
+					title: "Dropdown Page",
+					icon: "fa fa-list-ul",
+					child: [
+						{
+							href: "/page/sub-page-1",
+							title: "Sub Page 01",
+							icon: "fa fa-file-alt"
+						},
+						{
+							href: "/page/sub-page-2",
+							title: "Sub Page 02",
+							icon: "fa fa-file-alt"
+						}
+					]
+				},
+				{
+					title: "Multiple Level",
+					icon: "fa fa-list-alt",
+					child: [
+						{
+							title: "page"
+						},
+						{
+							title: "Level 2 ",
+							child: [
+								{
+									title: "page"
+								},
+								{
+									title: "Page"
+								}
+							]
+						},
+						{
+							title: "Page"
+						},
+						{
+							title: "Another Level 2",
+							child: [
+								{
+									title: "Level 3",
+									child: [
+										{
+											title: "Page"
+										},
+										{
+											title: "Page"
+										}
+									]
+								}
+							]
+						}
+					]
+				}
+			]
+		};
 	}
 };
 </script>
