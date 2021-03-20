290340f8f2e1859720a627f6bab766e1db0263b8
hong <tlsdoakstp@gmail.com>
2021-03-21 00:49:04 +0900

    설비-네트워크현황 #6
    html 작업
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- amivue/src/pages/Menu/Device/Network.vue
+++ amivue/src/pages/Menu/Device/Network.vue
@@ -1,19 +1,162 @@
 <template>
-	<div>
-		<b-button v-b-modal.modal-multi-1>Open First Modal</b-button>
-
-		<b-modal id="modal-multi-1" size="lg" title="First Modal" ok-only no-stacking>
-			<p class="my-2">First Modal</p>
-			<b-button v-b-modal.modal-multi-2>Open Second Modal</b-button>
-		</b-modal>
-
-		<b-modal id="modal-multi-2" title="Second Modal" ok-only>
-			<p class="my-2">Second Modal</p>
-			<b-button v-b-modal.modal-multi-3 size="sm">Open Third Modal</b-button>
-		</b-modal>
-
-		<b-modal id="modal-multi-3" size="sm" title="Third Modal" ok-only>
-			<p class="my-1">Third Modal</p>
-		</b-modal>
+	<div class="main-bg content">
+		<div class="main-location-wrap">
+			<h1>네트워크 현황</h1>
+			<div class="main-location">
+				<b-breadcrumb>
+					<b-breadcrumb-item to="/dashboard">
+						<b-icon icon="house" scale="1.25" shift-v="1.25" aria-hidden="true"></b-icon>
+						홈
+					</b-breadcrumb-item>
+					<b-breadcrumb-item>설비</b-breadcrumb-item>
+					<b-breadcrumb-item active>동 관리</b-breadcrumb-item>
+				</b-breadcrumb>
+			</div>
+		</div>
+		<div class="search-wrap2">
+			<div class="wbox">
+				<div class="search-img">
+					<b-icon icon="search" variant="primary"></b-icon>
+				</div>
+				<div class="search">
+					<!--검색영역-->
+					<form>
+						<b-row>
+							<b-col cols="4" lg="4">
+								<b-row class="form-group">
+									<b-col lg="4">
+										<label class="d-block">지역 코드</label>
+									</b-col>
+									<b-col lg="8">
+										<b-form-select v-model="selected" class="form-control">
+											<b-form-select-option>서울시</b-form-select-option>
+											<b-form-select-option>경기도</b-form-select-option>
+										</b-form-select>
+									</b-col>
+								</b-row>
+							</b-col>
+							<b-col cols="4" lg="4">
+								<b-row class="form-group">
+									<b-col lg="4">
+										<label class="d-block">단지 명</label>
+									</b-col>
+									<b-col lg="8">
+										<b-form-input v-model="text" placeholder="그랑시아 아파트" class="form-control"></b-form-input>
+									</b-col>
+								</b-row>
+							</b-col>
+							<b-col cols="4" lg="4">
+								<b-row class="form-group">
+									<b-col lg="4">
+										<label class="d-block">검침 타입</label>
+									</b-col>
+									<b-col lg="8">
+										<b-form-select v-model="selected" class="form-control">
+											<b-form-select-option>전기</b-form-select-option>
+											<b-form-select-option>수도</b-form-select-option>
+										</b-form-select>
+									</b-col>
+								</b-row>
+							</b-col>
+							<b-col cols="4" lg="4">
+								<b-row class="form-group">
+									<b-col lg="4">
+										<label class="d-block">DCU ID 또는 Gateway</label>
+									</b-col>
+									<b-col lg="8">
+										<b-form-input v-model="text" placeholder="" class="form-control"></b-form-input>
+									</b-col>
+								</b-row>
+							</b-col>
+						</b-row>
+					</form>
+					<!--//검색영역-->
+				</div>
+				<div class="btn-wrap ml-auto">
+					<b-button variant="primary btn-block">검색</b-button>
+				</div>
+			</div>
+		</div>
+		<div class="btn-wrap">
+			<div role="group" class="btn-group">
+				<span class="small">2021-01-20 12:15 기준</span>
+			</div>
+			<div role="group" class="btn-group">
+				<b-button variant="outline-secondary"><!--svg icon-->엑셀 다운로드</b-button>
+			</div>
+		</div>
+		<div class="table-wrap">
+			<table class="table bgtable b-table">
+				<thead>
+					<tr>
+						<th><div>지역</div></th>
+						<th><div>단지명</div></th>
+						<th><div>DCU ID</div></th>
+						<th><div>IP</div></th>
+						<th><div>Ping 평균</div></th>
+						<th><div>Ping 최소</div></th>
+						<th><div>Ping 최대</div></th>
+						<th><div>상태</div></th>
+						<th><div>DCU 통신</div></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td rowspan="2">서울</td>
+						<td rowspan="2">서울 아파트</td>
+						<td class="br" rowspan="2">NS09_010A</td>
+						<td>20.101.235.254</td>
+						<td>10ms</td>
+						<td>2ms</td>
+						<td>15ms</td>
+						<td class="br"><span class="linkage"></span></td>
+						<td rowspan="2"><span class="linkage"></span></td>
+					</tr>
+					<tr>
+						<td>20.101.235.254</td>
+						<td>10ms</td>
+						<td>2ms</td>
+						<td>15ms</td>
+						<td><span class="linkage"></span></td>
+					</tr>
+					<tr>
+						<td rowspan="2">서울</td>
+						<td rowspan="2">서울 아파트</td>
+						<td class="br" rowspan="2">NS09_010A</td>
+						<td>20.101.235.254</td>
+						<td>10ms</td>
+						<td>2ms</td>
+						<td>15ms</td>
+						<td class="br"><span class="linkage"></span></td>
+						<td rowspan="2"><span class="linkage"></span></td>
+					</tr>
+					<tr>
+						<td>20.101.235.254</td>
+						<td>10ms</td>
+						<td>2ms</td>
+						<td>15ms</td>
+						<td><span class="linkage"></span></td>
+					</tr>
+					<tr>
+						<td rowspan="2">서울</td>
+						<td rowspan="2">서울 아파트</td>
+						<td class="br" rowspan="2">NS09_010A</td>
+						<td>20.101.235.254</td>
+						<td>10ms</td>
+						<td>2ms</td>
+						<td>15ms</td>
+						<td class="br"><span class="linkage"></span></td>
+						<td rowspan="2"><span class="linkage"></span></td>
+					</tr>
+					<tr>
+						<td>20.101.235.254</td>
+						<td>10ms</td>
+						<td>2ms</td>
+						<td>15ms</td>
+						<td><span class="linkage"></span></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 	</div>
 </template>
