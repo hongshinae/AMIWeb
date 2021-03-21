df81e03f14b4d94d183825700615e24ae3f30849
hong <tlsdoakstp@gmail.com>
2021-03-22 03:06:20 +0900

    설비-매핑관리 #5
    html 모달 추가
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- amivue/src/pages/Menu/Device/Mapping.vue
+++ amivue/src/pages/Menu/Device/Mapping.vue
@@ -104,7 +104,7 @@
 			<b-col col lg="8">
 				<div class="btn-wrap">
 					<div role="group" class="btn-group">
-						<b-button variant="primary" v-b-modal.modal-1><b-icon icon="pencil-fill"></b-icon>DCU 신규등록</b-button>
+						<b-button variant="primary" v-b-modal.modal-1.mapping-modal-1><b-icon icon="pencil-fill"></b-icon>신규등록</b-button>
 					</div>
 					<div role="group" class="btn-group">
 						<b-button variant="outline-secondary"><!--svg icon-->엑셀 다운로드</b-button>
@@ -217,6 +217,178 @@
 				</div>
 			</b-col>
 		</b-row>
+		<b-modal id="mapping-modal-1" title="매핑정보 신규 등록">
+			<!---->
+			<div class="modal-content-wrap">
+				<form>
+					<b-row align-h="center">
+						<div class="modal-4th-box">
+							<div class="m-b-20">
+								<form class="one-row">
+									<b-row class="form-group">
+										<b-col lg="4">
+											<label class="d-block">검침 타입</label>
+										</b-col>
+										<b-col lg="8">
+											<b-form-select v-model="selected" class="form-control">
+												<b-form-select-option>전기</b-form-select-option>
+												<b-form-select-option>수도</b-form-select-option>
+											</b-form-select>
+										</b-col>
+									</b-row>
+									<b-row class="form-group">
+										<b-col lg="4">
+											<label class="d-block">지역 코드</label>
+										</b-col>
+										<b-col lg="8">
+											<b-form-select v-model="selected" class="form-control">
+												<b-form-select-option>서울시</b-form-select-option>
+												<b-form-select-option>경기도</b-form-select-option>
+											</b-form-select>
+										</b-col>
+									</b-row>
+									<b-row class="form-group">
+										<b-col lg="4">
+											<label class="d-block">단지 명</label>
+										</b-col>
+										<b-col lg="8">
+											<b-form-select v-model="selected" class="form-control">
+												<b-form-select-option>서울 아파트</b-form-select-option>
+												<b-form-select-option>판교 아파트</b-form-select-option>
+											</b-form-select>
+										</b-col>
+									</b-row>
+									<b-row class="form-group">
+										<b-col lg="4"> </b-col>
+										<b-col lg="8">
+											<b-button variant="primary btn-block">검색</b-button>
+										</b-col>
+									</b-row>
+								</form>
+							</div>
+							<div>
+								<div class="table-wrap">
+									<table class="table b-table basic-table" id="">
+										<thead role="rowgroup" class="">
+											<tr role="row" class="">
+												<th class=""><div>동</div></th>
+												<th class=""><div>호수</div></th>
+												<th class=""><div>Meter ID</div></th>
+												<th class=""><div>검침일</div></th>
+											</tr>
+										</thead>
+										<tbody role="rowgroup">
+											<tr role="row" class="">
+												<td class="">
+													<b-form-input v-model="text" placeholder="1006동" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="2020-12-12 17:21:28" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="1542541" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="15일" class="form-control"></b-form-input>
+												</td>
+											</tr>
+											<tr role="row" class="">
+												<td class="">
+													<b-form-input v-model="text" placeholder="1006동" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="2020-12-12 17:21:28" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="1542541" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="15일" class="form-control"></b-form-input>
+												</td>
+											</tr>
+											<tr role="row" class="">
+												<td class="">
+													<b-form-input v-model="text" placeholder="1006동" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="2020-12-12 17:21:28" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="1542541" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="15일" class="form-control"></b-form-input>
+												</td>
+											</tr>
+											<tr role="row" class="">
+												<td class="">
+													<b-form-input v-model="text" placeholder="1006동" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="2020-12-12 17:21:28" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="1542541" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="15일" class="form-control"></b-form-input>
+												</td>
+											</tr>
+											<tr role="row" class="">
+												<td class="">
+													<b-form-input v-model="text" placeholder="1006동" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="2020-12-12 17:21:28" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="1542541" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="15일" class="form-control"></b-form-input>
+												</td>
+											</tr>
+											<tr role="row" class="">
+												<td class="">
+													<b-form-input v-model="text" placeholder="1006동" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="2020-12-12 17:21:28" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="1542541" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="15일" class="form-control"></b-form-input>
+												</td>
+											</tr>
+											<tr role="row" class="">
+												<td class="">
+													<b-form-input v-model="text" placeholder="1006동" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="2020-12-12 17:21:28" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="1542541" class="form-control"></b-form-input>
+												</td>
+												<td class="">
+													<b-form-input v-model="text" placeholder="15일" class="form-control"></b-form-input>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+								<div class="pa-wrap">
+									<b-pagination v-model="currentPage" :total-rows="rows" size="sm"></b-pagination>
+								</div>
+							</div>
+						</div>
+					</b-row>
+				</form>
+			</div>
+			<!---->
+		</b-modal>
 	</div>
 </template>
 <script></script>
