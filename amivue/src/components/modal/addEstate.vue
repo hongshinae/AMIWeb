0482f7db3896e17b596278e4e1b3c3762c2b30ff
hong <tlsdoakstp@gmail.com>
2021-03-25 12:02:52 +0900

    메인페이지 구현 (공통포함) #1
    <b-form-group>으로 변경
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- amivue/src/components/modal/addEstate.vue
+++ amivue/src/components/modal/addEstate.vue
@@ -4,105 +4,105 @@
 			<form>
 				<b-row align-h="center">
 					<div class="modal-1st-box">
-						<div class="form-group">
+						<b-form-group>
 							<label class="d-block label-wrap">단지 ID</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="단지 ID" class="form-control"></b-form-input>
 							</div>
-						</div>
-						<div class="form-group">
+						</b-form-group>
+						<b-form-group>
 							<label class="d-block label-wrap">세대 수</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="세대 수" class="form-control"></b-form-input>
 							</div>
-						</div>
-						<div class="form-group">
+						</b-form-group>
+						<b-form-group>
 							<label class="d-block label-wrap">지역</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="지역" class="form-control"></b-form-input>
 							</div>
-						</div>
-						<div class="form-group">
+						</b-form-group>
+						<b-form-group>
 							<label class="d-block label-wrap">담당자 1</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="담당자 1" class="form-control"></b-form-input>
 							</div>
-						</div>
-						<div class="form-group">
+						</b-form-group>
+						<b-form-group>
 							<label class="d-block label-wrap">담당자 2</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="담당자 2" class="form-control"></b-form-input>
 							</div>
-						</div>
-						<div class="form-group">
+						</b-form-group>
+						<b-form-group>
 							<label class="d-block label-wrap">설치 DCU 수</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="설치 DCU 수" class="form-control"></b-form-input>
 							</div>
-						</div>
-						<div class="form-group">
+						</b-form-group>
+						<b-form-group>
 							<label class="d-block label-wrap">설치 Meter 수</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="설치 Meter 수" class="form-control"></b-form-input>
 							</div>
-						</div>
-						<div class="form-group">
+						</b-form-group>
+						<b-form-group>
 							<label class="d-block label-wrap">수도 Meter 수</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="수도 Meter 수" class="form-control"></b-form-input>
 							</div>
-						</div>
-						<div class="form-group">
+						</b-form-group>
+						<b-form-group>
 							<label class="d-block label-wrap">기타</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="기타" class="form-control"></b-form-input>
 							</div>
-						</div>
+						</b-form-group>
 					</div>
 					<div class="modal-2nd-box">
-						<div class="form-group">
+						<b-form-group>
 							<label class="d-block label-wrap">단지 명</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="삼성단지" class="form-control"></b-form-input>
 							</div>
-						</div>
-						<div class="form-group">
+						</b-form-group>
+						<b-form-group>
 							<label class="d-block label-wrap">주소</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="경기도 성남시 분당구 삼평동 1006동 210호" class="form-control"></b-form-input>
 							</div>
-						</div>
-						<div class="form-group">
+						</b-form-group>
+						<b-form-group>
 							<label class="d-block label-wrap">연락처</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="010.1212.1211" class="form-control"></b-form-input>
 							</div>
-						</div>
-						<div class="form-group">
+						</b-form-group>
+						<b-form-group>
 							<label class="d-block label-wrap">담당자 1 연락처</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="담당자 1 연락처" class="form-control"></b-form-input>
 							</div>
-						</div>
-						<div class="form-group">
+						</b-form-group>
+						<b-form-group>
 							<label class="d-block label-wrap">담당자 2 연락처</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="담당자 2 연락처" class="form-control"></b-form-input>
 							</div>
-						</div>
-						<div class="form-group">
+						</b-form-group>
+						<b-form-group>
 							<label class="d-block label-wrap">설치 모뎀 수</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="설치 모뎀 수" class="form-control"></b-form-input>
 							</div>
-						</div>
-						<div class="form-group">
+						</b-form-group>
+						<b-form-group>
 							<label class="d-block label-wrap">가스 Meter 수</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="가스 Meter 수" class="form-control"></b-form-input>
 							</div>
-						</div>
-						<div class="form-group">
+						</b-form-group>
+						<b-form-group>
 							<label class="d-block label-wrap">동 호수</label>
 							<div class="input-wrap">
 								<label>1006 동</label>
@@ -110,16 +110,16 @@
 								<label>104 호</label>
 								<b-button variant="primary" class="btn btn-light">호 선택</b-button>
 							</div>
-						</div>
-						<div class="form-group">
+						</b-form-group>
+						<b-form-group>
 							<label class="d-block label-wrap">기타</label>
 							<div class="input-wrap">
 								<b-form-input placeholder="기타" class="form-control"></b-form-input>
 							</div>
-						</div>
+						</b-form-group>
 					</div>
 					<div class="modal-3rd-box">
-						<div class="form-group">
+						<b-form-group>
 							<label class="d-block label-wrap">검침 타입</label>
 							<div class="input-wrap">
 								<ul class="modal-ul">
@@ -130,10 +130,10 @@
 									<li><b-form-checkbox></b-form-checkbox><label>난방</label></li>
 								</ul>
 							</div>
-						</div>
+						</b-form-group>
 					</div>
 					<div class="modal-4th-box">
-						<div class="form-group">
+						<b-form-group>
 							<label class="d-block label-wrap">검침 일</label>
 							<ul class="bg">
 								<li>전기 <i class="p-Color">15</i>일</li>
@@ -143,7 +143,7 @@
 								<li>난방 <i class="p-Color">5</i>일</li>
 							</ul>
 							<b-button variant="primary" class="btn btn-light">단지 검침일 일괄 변경</b-button>
-						</div>
+						</b-form-group>
 					</div>
 				</b-row>
 			</form>
--- amivue/src/pages/Menu/Device/Building.vue
+++ amivue/src/pages/Menu/Device/Building.vue
@@ -50,6 +50,138 @@
 				<b-button class="btn-light btn-excel"><!--svg icon-->엑셀 다운로드</b-button>
 			</div>
 		</div>
+		<div class="filter-wrap">
+			<b-row>
+				<b-col lg="2">
+					<b-form-group>
+						<label class="d-block label-wrap">지역 선택</label>
+						<b-input-group>
+							<b-form-select v-model="selected" class="form-control">
+								<b-form-select-option>서울시</b-form-select-option>
+								<b-form-select-option>경기도</b-form-select-option>
+							</b-form-select>
+							<b-form-select v-model="selected" class="form-control">
+								<b-form-select-option>서울시</b-form-select-option>
+								<b-form-select-option>경기도</b-form-select-option>
+							</b-form-select>
+						</b-input-group>
+					</b-form-group>
+				</b-col>
+				<b-col lg="2">
+					<b-form-group>
+						<label class="d-block label-wrap">단지 선택</label>
+						<div class="input-wrap">
+							<b-form-select v-model="selected" class="form-control">
+								<b-form-select-option>서울시</b-form-select-option>
+								<b-form-select-option>경기도</b-form-select-option>
+							</b-form-select>
+						</div>
+					</b-form-group>
+				</b-col>
+				<div class="my-1 col-lg-6">
+					<div role="group" class="form-row form-group mb-0" id="__BVID__1341">
+						<label for="sort-by-select" class="col-sm-3 col-form-label col-form-label-sm text-sm-right" id="__BVID__1341__BV_label_">Sort</label>
+						<div class="col">
+							<div role="group" class="input-group input-group-sm">
+								<!----><select id="sort-by-select" class="w-75 custom-select"
+									><option value="">-- none --</option
+									><option value="name">Person full name</option
+									><option value="age">Person age</option
+									><option value="isActive">Is Active</option></select
+								>
+								<select class="w-25 custom-select custom-select-sm" id="__BVID__1343"
+									><option value="false">Asc</option>
+									<option value="true">Desc</option></select
+								><!---->
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="my-1 col-lg-6">
+					<div role="group" class="form-row form-group mb-0" id="__BVID__1344">
+						<label for="initial-sort-select" class="col-sm-3 col-form-label col-form-label-sm text-sm-right" id="__BVID__1344__BV_label_"
+							>Initial sort</label
+						>
+						<div class="col">
+							<select id="initial-sort-select" class="custom-select custom-select-sm"
+								><option value="asc">asc</option
+								><option value="desc">desc</option
+								><option value="last">last</option></select
+							>
+						</div>
+					</div>
+				</div>
+				<div class="my-1 col-lg-6">
+					<div role="group" class="form-row form-group mb-0" id="__BVID__1346">
+						<label for="filter-input" class="col-sm-3 col-form-label col-form-label-sm text-sm-right" id="__BVID__1346__BV_label_">Filter</label>
+						<div class="col">
+							<div role="group" class="input-group input-group-sm">
+								<!----><input id="filter-input" type="search" placeholder="Type to Search" class="form-control" />
+								<div class="input-group-append">
+									<button type="button" class="btn btn-secondary disabled" disabled="disabled">Clear</button>
+								</div>
+								<!---->
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="my-1 col-lg-6">
+					<fieldset class="form-group mb-0" value="desc" id="__BVID__1348" aria-labelledby="__BVID__1348__BV_label_">
+						<div class="form-row">
+							<legend tabindex="-1" class="col-sm-3 bv-no-focus-ring col-form-label col-form-label-sm text-sm-right" id="__BVID__1348__BV_label_">
+								Filter On
+							</legend>
+							<div class="col">
+								<div role="group" tabindex="-1" class="mt-1 bv-no-focus-ring" id="__BVID__1349">
+									<div class="custom-control custom-control-inline custom-checkbox">
+										<input type="checkbox" class="custom-control-input" value="name" id="__BVID__1350" name="__BVID__1349" /><label
+											class="custom-control-label"
+											for="__BVID__1350"
+											>Name</label
+										>
+									</div>
+									<div class="custom-control custom-control-inline custom-checkbox">
+										<input type="checkbox" class="custom-control-input" value="age" id="__BVID__1351" name="__BVID__1349" /><label
+											class="custom-control-label"
+											for="__BVID__1351"
+											>Age</label
+										>
+									</div>
+									<div class="custom-control custom-control-inline custom-checkbox">
+										<input type="checkbox" class="custom-control-input" value="isActive" id="__BVID__1352" name="__BVID__1349" /><label
+											class="custom-control-label"
+											for="__BVID__1352"
+											>Active</label
+										>
+									</div>
+								</div>
+								<!----><!----><small tabindex="-1" class="form-text text-muted" id="__BVID__1348__BV_description_"
+									>Leave all unchecked to filter on all data</small
+								>
+							</div>
+						</div>
+					</fieldset>
+				</div>
+				<div class="my-1 col-sm-5 col-md-6">
+					<div role="group" class="form-row form-group mb-0" id="__BVID__1353">
+						<label
+							for="per-page-select"
+							class="col-sm-6 col-md-4 col-lg-3 col-form-label col-form-label-sm text-sm-right"
+							id="__BVID__1353__BV_label_"
+							>Per page</label
+						>
+						<div class="col">
+							<select id="per-page-select" class="custom-select custom-select-sm"
+								><option value="5">5</option
+								><option value="10">10</option
+								><option value="15">15</option
+								><option value="100">Show a lot</option></select
+							><!----><!----><!---->
+						</div>
+					</div>
+				</div>
+			</b-row>
+		</div>
 		<div class="table-wrap">
 			<table class="table bgtable b-table table-striped">
 				<thead>
@@ -117,7 +249,7 @@
 				<form>
 					<b-row align-h="center">
 						<div class="modal-4th-box">
-							<div class="form-group">
+							<b-form-group>
 								<label class="d-block label-wrap">지역 선택</label>
 								<div class="input-wrap">
 									<b-form-select v-model="selected" class="form-control">
@@ -125,8 +257,8 @@
 										<b-form-select-option>경기도</b-form-select-option>
 									</b-form-select>
 								</div>
-							</div>
-							<div class="form-group">
+							</b-form-group>
+							<b-form-group>
 								<label class="d-block label-wrap">단지 선택</label>
 								<div class="input-wrap">
 									<b-form-select v-model="selected" class="form-control">
@@ -134,14 +266,14 @@
 										<b-form-select-option>경기도</b-form-select-option>
 									</b-form-select>
 								</div>
-							</div>
-							<div class="form-group">
+							</b-form-group>
+							<b-form-group>
 								<label class="d-block label-wrap">등록 동명</label>
 								<div class="input-wrap">
 									<b-form-input v-model="text" placeholder="404동" class="form-control"></b-form-input>
 								</div>
-							</div>
-							<div class="form-group">
+							</b-form-group>
+							<b-form-group>
 								<label class="d-block label-wrap">DCU ID</label>
 								<div class="input-wrap">
 									<ul>
@@ -149,7 +281,7 @@
 										<li><b-button class="btn" variant="outline-primary">연동확인</b-button></li>
 									</ul>
 								</div>
-							</div>
+							</b-form-group>
 						</div>
 					</b-row>
 				</form>
