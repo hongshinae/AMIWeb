a38661d646956a18ebe15fd268d87d64db5e77e3
hong <tlsdoakstp@gmail.com>
2021-03-27 01:35:39 +0900

    설비-네트워크현황 #6
    필터추가, v-button 변경
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- amivue/src/pages/Menu/Device/Network.vue
+++ amivue/src/pages/Menu/Device/Network.vue
@@ -22,10 +22,10 @@
 					<!--검색영역-->
 					<form>
 						<b-row>
-							<b-col cols="4" lg="4">
+							<b-col cols="4" xl="3">
 								<b-row class="form-group">
 									<b-col lg="4">
-										<label class="d-block">지역 코드</label>
+										<label class="d-block">지역 이름</label>
 									</b-col>
 									<b-col lg="8">
 										<b-form-select v-model="selected" class="form-control">
@@ -35,17 +35,20 @@
 									</b-col>
 								</b-row>
 							</b-col>
-							<b-col cols="4" lg="4">
+							<b-col cols="4" xl="3">
 								<b-row class="form-group">
 									<b-col lg="4">
 										<label class="d-block">단지 명</label>
 									</b-col>
 									<b-col lg="8">
-										<b-form-input v-model="text" placeholder="그랑시아 아파트" class="form-control"></b-form-input>
+										<b-form-select v-model="selected" class="form-control">
+											<b-form-select-option>1단지</b-form-select-option>
+											<b-form-select-option>2단지</b-form-select-option>
+										</b-form-select>
 									</b-col>
 								</b-row>
 							</b-col>
-							<b-col cols="4" lg="4">
+							<b-col cols="4" xl="3">
 								<b-row class="form-group">
 									<b-col lg="4">
 										<label class="d-block">검침 타입</label>
@@ -53,18 +56,20 @@
 									<b-col lg="8">
 										<b-form-select v-model="selected" class="form-control">
 											<b-form-select-option>전기</b-form-select-option>
-											<b-form-select-option>수도</b-form-select-option>
+											<b-form-select-option>난방</b-form-select-option>
 										</b-form-select>
 									</b-col>
 								</b-row>
 							</b-col>
-							<b-col cols="4" lg="4">
+						</b-row>
+						<b-row>
+							<b-col cols="4" xl="3">
 								<b-row class="form-group">
 									<b-col lg="4">
 										<label class="d-block">DCU ID 또는 Gateway</label>
 									</b-col>
 									<b-col lg="8">
-										<b-form-input v-model="text" placeholder="" class="form-control"></b-form-input>
+										<b-form-input v-model="text" placeholder="DCU ID 또는 Gateway" class="form-control"></b-form-input>
 									</b-col>
 								</b-row>
 							</b-col>
@@ -73,90 +78,120 @@
 					<!--//검색영역-->
 				</div>
 				<div class="btn-wrap ml-auto">
-					<b-button variant="primary btn-block">검색</b-button>
+					<b-button block variant="primary">검색</b-button>
 				</div>
 			</div>
 		</div>
-		<div class="btn-wrap">
-			<div role="group" class="btn-group">
+		<div class="btn-filter-wrap">
+			<div class="btn-wrap">
+				<b-button-group>
+					<b-button variant="light btn-excel">엑셀 다운로드</b-button>
+				</b-button-group>
 				<span class="small">2021-01-20 12:15 기준</span>
 			</div>
-			<div role="group" class="btn-group">
-				<b-button class="btn-light btn-excel"><!--svg icon-->엑셀 다운로드</b-button>
+			<div class="filter-wrap">
+				<b-form-group id="" label="지역">
+					<b-form-select v-model="selected" class="form-control">
+						<b-form-select-option>서울</b-form-select-option>
+						<b-form-select-option>경기</b-form-select-option>
+					</b-form-select>
+				</b-form-group>
+				<b-form-group id="" label="단지 명">
+					<b-form-select v-model="selected" class="form-control">
+						<b-form-select-option>1단지</b-form-select-option>
+						<b-form-select-option>2단지</b-form-select-option>
+					</b-form-select>
+				</b-form-group>
+				<b-form-group id="" label="DCU ID">
+					<b-form-select v-model="selected">
+						<b-form-select-option>NS09_011A</b-form-select-option>
+						<b-form-select-option>NS09_01124A</b-form-select-option>
+						<b-form-select-option>NS09_01511A</b-form-select-option>
+					</b-form-select>
+				</b-form-group>
+				<b-form-group id="">
+					<b-form-select v-model="selected" class="form-control">
+						<b-form-select-option>5개씩 보기</b-form-select-option>
+						<b-form-select-option>10개씩 보기</b-form-select-option>
+						<b-form-select-option>50개씩 보기</b-form-select-option>
+					</b-form-select>
+				</b-form-group>
 			</div>
 		</div>
 		<div class="table-wrap">
-			<table class="table bgtable b-table">
-				<thead>
-					<tr>
-						<th><div>지역</div></th>
-						<th><div>단지명</div></th>
-						<th><div>DCU ID</div></th>
-						<th><div>IP</div></th>
-						<th><div>Ping 평균</div></th>
-						<th><div>Ping 최소</div></th>
-						<th><div>Ping 최대</div></th>
-						<th><div>상태</div></th>
-						<th><div>DCU 통신</div></th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td rowspan="2">서울</td>
-						<td rowspan="2">서울 아파트</td>
-						<td class="br" rowspan="2">NS09_010A</td>
-						<td>20.101.235.254</td>
-						<td>10ms</td>
-						<td>2ms</td>
-						<td>15ms</td>
-						<td class="br"><span class="linkage"></span></td>
-						<td rowspan="2"><span class="linkage"></span></td>
-					</tr>
-					<tr>
-						<td>20.101.235.254</td>
-						<td>10ms</td>
-						<td>2ms</td>
-						<td>15ms</td>
-						<td><span class="linkage"></span></td>
-					</tr>
-					<tr>
-						<td rowspan="2">서울</td>
-						<td rowspan="2">서울 아파트</td>
-						<td class="br" rowspan="2">NS09_010A</td>
-						<td>20.101.235.254</td>
-						<td>10ms</td>
-						<td>2ms</td>
-						<td>15ms</td>
-						<td class="br"><span class="linkage"></span></td>
-						<td rowspan="2"><span class="linkage"></span></td>
-					</tr>
-					<tr>
-						<td>20.101.235.254</td>
-						<td>10ms</td>
-						<td>2ms</td>
-						<td>15ms</td>
-						<td><span class="linkage"></span></td>
-					</tr>
-					<tr>
-						<td rowspan="2">서울</td>
-						<td rowspan="2">서울 아파트</td>
-						<td class="br" rowspan="2">NS09_010A</td>
-						<td>20.101.235.254</td>
-						<td>10ms</td>
-						<td>2ms</td>
-						<td>15ms</td>
-						<td class="br"><span class="linkage"></span></td>
-						<td rowspan="2"><span class="linkage"></span></td>
-					</tr>
-					<tr>
-						<td>20.101.235.254</td>
-						<td>10ms</td>
-						<td>2ms</td>
-						<td>15ms</td>
-						<td><span class="linkage"></span></td>
-					</tr>
-				</tbody>
-			</table>
+			<div class="bgtable">
+				<table class="table b-table table-striped">
+					<thead>
+						<tr>
+							<th><div>지역</div></th>
+							<th><div>단지명</div></th>
+							<th><div>DCU ID</div></th>
+							<th><div>IP</div></th>
+							<th><div>Ping 평균</div></th>
+							<th><div>Ping 최소</div></th>
+							<th><div>Ping 최대</div></th>
+							<th><div>상태</div></th>
+							<th><div>DCU 통신</div></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td rowspan="2">서울</td>
+							<td rowspan="2">서울 아파트</td>
+							<td class="br" rowspan="2">NS09_010A</td>
+							<td>20.101.235.254</td>
+							<td>10ms</td>
+							<td>2ms</td>
+							<td>15ms</td>
+							<td class="br"><span class="linkage"></span></td>
+							<td rowspan="2"><span class="linkage"></span></td>
+						</tr>
+						<tr>
+							<td>20.101.235.254</td>
+							<td>10ms</td>
+							<td>2ms</td>
+							<td>15ms</td>
+							<td><span class="linkage"></span></td>
+						</tr>
+						<tr>
+							<td rowspan="2">서울</td>
+							<td rowspan="2">서울 아파트</td>
+							<td class="br" rowspan="2">NS09_010A</td>
+							<td>20.101.235.254</td>
+							<td>10ms</td>
+							<td>2ms</td>
+							<td>15ms</td>
+							<td class="br"><span class="linkage"></span></td>
+							<td rowspan="2"><span class="linkage"></span></td>
+						</tr>
+						<tr>
+							<td>20.101.235.254</td>
+							<td>10ms</td>
+							<td>2ms</td>
+							<td>15ms</td>
+							<td><span class="linkage"></span></td>
+						</tr>
+						<tr>
+							<td rowspan="2">서울</td>
+							<td rowspan="2">서울 아파트</td>
+							<td class="br" rowspan="2">NS09_010A</td>
+							<td>20.101.235.254</td>
+							<td>10ms</td>
+							<td>2ms</td>
+							<td>15ms</td>
+							<td class="br"><span class="linkage"></span></td>
+							<td rowspan="2"><span class="linkage"></span></td>
+						</tr>
+						<tr>
+							<td>20.101.235.254</td>
+							<td>10ms</td>
+							<td>2ms</td>
+							<td>15ms</td>
+							<td><span class="linkage"></span></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</div>
 	</div>
 </template>
