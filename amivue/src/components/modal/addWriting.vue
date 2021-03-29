3d3a067fa93f444663f0343ef8eb095489f612b8
hong <tlsdoakstp@gmail.com>
2021-03-29 14:06:49 +0900

    메인페이지 구현 (공통포함) #1
    고객지원 테스트
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

---
+++ amivue/src/components/modal/addWriting.vue
@@ -0,0 +1,46 @@
+<template>
+	<b-modal id="addWriting" title="글쓰기">
+		<!---->
+		<div class="modal-content-wrap">
+			<form>
+				<b-row align-h="center">
+					<div class="modal-box">
+						<b-form-group label="지역 선택" label-for="input1">
+							<b-form-select v-model="selected">
+								<b-form-select-option>서울시</b-form-select-option>
+								<b-form-select-option>경기도</b-form-select-option>
+							</b-form-select>
+						</b-form-group>
+
+						<b-form-group label="단지 선택" label-for="">
+							<b-form-select v-model="selected">
+								<b-form-select-option>서울시</b-form-select-option>
+								<b-form-select-option>경기도</b-form-select-option>
+							</b-form-select>
+						</b-form-group>
+
+						<b-form-group label="등록 동명" label-for="">
+							<b-form-input id="" placeholder="404동"></b-form-input>
+						</b-form-group>
+
+						<b-form-group label="DCU ID" label-for="">
+							<b-input-group>
+								<b-form-input v-model="text" placeholder="4521542" class="form-control"></b-form-input>
+								<b-input-group-append>
+									<b-button variant="light">연동 확인</b-button>
+								</b-input-group-append>
+							</b-input-group>
+						</b-form-group>
+					</div>
+				</b-row>
+			</form>
+		</div>
+		<!---->
+	</b-modal>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ amivue/src/pages/Menu/Support/QnA.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="main-bg content">
+		<div class="main-location-wrap">
+			<h1>Q&A</h1>
+			<div class="main-location">
+				<b-breadcrumb>
+					<b-breadcrumb-item to="/dashboard">
+						<b-icon icon="house" scale="1.25" shift-v="1.25" aria-hidden="true"></b-icon>
+						홈
+					</b-breadcrumb-item>
+					<b-breadcrumb-item>고객지원</b-breadcrumb-item>
+					<b-breadcrumb-item active>Q&A</b-breadcrumb-item>
+				</b-breadcrumb>
+			</div>
+		</div>
+
+		<div class="table-wrap">
+			<div>
+				<div>
+					<b-button block v-b-toggle.my-collapse01>1. kdkd</b-button>
+				</div>
+
+				<b-collapse id="my-collapse01">
+					1. 내용
+				</b-collapse>
+			</div>
+			<div>
+				<div>
+					<b-button block v-b-toggle.my-collapse02>2.</b-button>
+				</div>
+
+				<b-collapse id="my-collapse02">
+					1. 내용
+				</b-collapse>
+			</div>
+		</div>
+		<div>
+			<b-button v-b-toggle:my-collapse> <span class="when-open">Close</span><span class="when-closed">Open</span> My Collapse </b-button>
+			<b-collapse id="my-collapse">
+				<!-- Content here -->
+				fff
+			</b-collapse>
+		</div>
+		<div>
+			<b-button v-b-toggle:my-collapse> <span class="when-open">Close</span><span class="when-closed">Open</span> My Collapse </b-button>
+			<b-collapse id="my-collapse">
+				<!-- Content here -->
+				fff
+			</b-collapse>
+		</div>
+	</div>
+</template>
+<script></script>
+<style>
+.collapsed > .when-open,
+.not-collapsed > .when-closed {
+	display: none;
+}
+</style>
