c141f2e0ea1c263f2f232d61a2f37775551c3223
hong <tlsdoakstp@gmail.com>
2021-03-31 14:31:26 +0900

    설비-단지관리 구현 #2
    단지 관리 모달 html scss 수정작업.
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- amivue/src/components/modal/addEstate.vue
+++ amivue/src/components/modal/addEstate.vue
@@ -1,6 +1,6 @@
 <template>
 	<b-modal id="addEstate" title="단지 상세 관리" ok-only no-stacking>
-		<div class="modal-content-wrap">
+		<div class="center">
 			<div class="modal-1st-box">
 				<b-form-group label="단지 ID" label-for="">
 					<b-form-input id="" placeholder="단지 ID"></b-form-input>
@@ -8,153 +8,105 @@
 				<b-form-group label="세대 수" label-for="">
 					<b-form-input id="" placeholder="세대 수"></b-form-input>
 				</b-form-group>
-
 				<b-form-group label="지역 선택" label-for="input1">
 					<b-form-select>
-						<b-form-select-option value="0">서울시</b-form-select-option>
-						<b-form-select-option value="1">경기도</b-form-select-option>
+						<b-form-select-option>서울시</b-form-select-option>
+						<b-form-select-option>경기도</b-form-select-option>
 					</b-form-select>
 				</b-form-group>
-
-				<b-form-group>
-					<label class="d-block label-wrap">단지 ID</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="단지 ID" class="form-control"></b-form-input>
-					</div>
+				<b-form-group label="담당자1" label-for="">
+					<b-form-input id="" placeholder="담당자1"></b-form-input>
 				</b-form-group>
-				<b-form-group>
-					<label class="d-block label-wrap">세대 수</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="세대 수" class="form-control"></b-form-input>
-					</div>
+				<b-form-group label="담당자2" label-for="">
+					<b-form-input id="" placeholder="담당자2"></b-form-input>
 				</b-form-group>
-				<b-form-group>
-					<label class="d-block label-wrap">지역</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="지역" class="form-control"></b-form-input>
-					</div>
+				<b-form-group label="설치 DCU 수" label-for="">
+					<b-form-input id="" placeholder="설치 DCU 수"></b-form-input>
 				</b-form-group>
-				<b-form-group>
-					<label class="d-block label-wrap">담당자 1</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="담당자 1" class="form-control"></b-form-input>
-					</div>
+				<b-form-group label="설치 Meter 수" label-for="">
+					<b-form-input id="" placeholder="설치 Meter 수"></b-form-input>
 				</b-form-group>
-				<b-form-group>
-					<label class="d-block label-wrap">담당자 2</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="담당자 2" class="form-control"></b-form-input>
-					</div>
+				<b-form-group label="수도 Meter 수" label-for="">
+					<b-form-input id="" placeholder="수도 Meter 수"></b-form-input>
 				</b-form-group>
-				<b-form-group>
-					<label class="d-block label-wrap">설치 DCU 수</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="설치 DCU 수" class="form-control"></b-form-input>
-					</div>
-				</b-form-group>
-				<b-form-group>
-					<label class="d-block label-wrap">설치 Meter 수</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="설치 Meter 수" class="form-control"></b-form-input>
-					</div>
-				</b-form-group>
-				<b-form-group>
-					<label class="d-block label-wrap">수도 Meter 수</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="수도 Meter 수" class="form-control"></b-form-input>
-					</div>
-				</b-form-group>
-				<b-form-group>
-					<label class="d-block label-wrap">기타</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="기타" class="form-control"></b-form-input>
-					</div>
+				<b-form-group label="기타" label-for="">
+					<b-form-input id="" placeholder="기타"></b-form-input>
 				</b-form-group>
 			</div>
 			<div class="modal-2nd-box">
-				<b-form-group>
-					<label class="d-block label-wrap">단지 명</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="삼성단지" class="form-control"></b-form-input>
-					</div>
+				<b-form-group label="단지 명" label-for="">
+					<b-form-select>
+						<b-form-select-option>1단지</b-form-select-option>
+						<b-form-select-option>2단지</b-form-select-option>
+					</b-form-select>
 				</b-form-group>
-				<b-form-group>
-					<label class="d-block label-wrap">주소</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="경기도 성남시 분당구 삼평동 1006동 210호" class="form-control"></b-form-input>
-					</div>
+				<b-form-group label="주소" label-for="">
+					<b-form-input id="" placeholder="경기도 성남시 분당구 삼평동 1006동 210호"></b-form-input>
 				</b-form-group>
-				<b-form-group>
-					<label class="d-block label-wrap">연락처</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="010.1212.1211" class="form-control"></b-form-input>
-					</div>
+				<b-form-group label="연락처" label-for="">
+					<b-form-input id="" placeholder="010.1211.1111"></b-form-input>
 				</b-form-group>
-				<b-form-group>
-					<label class="d-block label-wrap">담당자 1 연락처</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="담당자 1 연락처" class="form-control"></b-form-input>
-					</div>
+				<b-form-group label="담당자1 연락처" label-for="">
+					<b-form-input id="" placeholder="담당자1 연락처"></b-form-input>
 				</b-form-group>
-				<b-form-group>
-					<label class="d-block label-wrap">담당자 2 연락처</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="담당자 2 연락처" class="form-control"></b-form-input>
-					</div>
+				<b-form-group label="담당자2 연락처" label-for="">
+					<b-form-input id="" placeholder="담당자2 연락처"></b-form-input>
 				</b-form-group>
-				<b-form-group>
-					<label class="d-block label-wrap">설치 모뎀 수</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="설치 모뎀 수" class="form-control"></b-form-input>
-					</div>
+				<b-form-group label="설치 모뎀 수" label-for="">
+					<b-form-input id="" placeholder="설치 모뎀 수"></b-form-input>
 				</b-form-group>
-				<b-form-group>
-					<label class="d-block label-wrap">가스 Meter 수</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="가스 Meter 수" class="form-control"></b-form-input>
-					</div>
+				<b-form-group label="가스 Meter 수" label-for="">
+					<b-form-input id="" placeholder="가스 Meter 수"></b-form-input>
 				</b-form-group>
-				<b-form-group>
-					<label class="d-block label-wrap">동 호수</label>
-					<div class="input-wrap">
-						<label>1006 동</label>
-						<b-button variant="primary" class="btn btn-light">동 선택</b-button>
-						<label>104 호</label>
-						<b-button variant="primary" class="btn btn-light">호 선택</b-button>
-					</div>
+				<b-form-group label="기타" label-for="">
+					<b-form-input id="" placeholder="기타"></b-form-input>
 				</b-form-group>
-				<b-form-group>
-					<label class="d-block label-wrap">기타</label>
-					<div class="input-wrap">
-						<b-form-input placeholder="기타" class="form-control"></b-form-input>
-					</div>
+				<b-form-group label="동 호수" label-for="">
+					<b-row>
+						<b-col col="6">
+							<b-input-group>
+								<b-form-input v-model="text" placeholder="1006동"></b-form-input>
+								<b-input-group-append>
+									<b-button variant="light">동 선택</b-button>
+								</b-input-group-append>
+							</b-input-group>
+						</b-col>
+						<b-col col="6">
+							<b-input-group>
+								<b-form-input v-model="text" placeholder="902호"></b-form-input>
+								<b-input-group-append>
+									<b-button variant="light">호 선택</b-button>
+								</b-input-group-append>
+							</b-input-group>
+						</b-col>
+					</b-row>
 				</b-form-group>
 			</div>
 			<div class="modal-3rd-box">
-				<b-form-group>
-					<label class="d-block label-wrap">검침 타입</label>
-					<div class="input-wrap">
-						<ul class="modal-ul">
-							<li><b-form-checkbox></b-form-checkbox><label>전기</label></li>
-							<li><b-form-checkbox></b-form-checkbox><label>가스</label></li>
-							<li><b-form-checkbox></b-form-checkbox><label>수도</label></li>
-							<li><b-form-checkbox></b-form-checkbox><label>온수</label></li>
-							<li><b-form-checkbox></b-form-checkbox><label>난방</label></li>
-						</ul>
-					</div>
+				<b-form-group label="검침 타입">
+					<b-form-checkbox-group>
+						<b-form-checkbox value="전기">전기</b-form-checkbox>
+						<b-form-checkbox value="가스">가스</b-form-checkbox>
+						<b-form-checkbox value="수도">수도</b-form-checkbox>
+						<b-form-checkbox value="온수">온수</b-form-checkbox>
+						<b-form-checkbox value="난방">난방</b-form-checkbox>
+					</b-form-checkbox-group>
 				</b-form-group>
 			</div>
 			<div class="modal-4th-box">
-				<b-form-group>
-					<label class="d-block label-wrap">검침 일</label>
-					<ul class="bg">
-						<li>전기 <i class="p-Color">15</i>일</li>
-						<li>가스 <i class="p-Color">1</i>일</li>
-						<li>수도 <i class="p-Color">4</i>일</li>
-						<li>온수 <i class="p-Color">1</i>일</li>
-						<li>난방 <i class="p-Color">5</i>일</li>
-					</ul>
-					<b-button variant="primary" class="btn btn-light">단지 검침일 일괄 변경</b-button>
+				<b-form-group label="검침일">
+					<b-input-group>
+						<ul class="bg">
+							<li>전기 <i class="p-Color">15</i>일</li>
+							<li>가스 <i class="p-Color">1</i>일</li>
+							<li>수도 <i class="p-Color">4</i>일</li>
+							<li>온수 <i class="p-Color">1</i>일</li>
+							<li>난방 <i class="p-Color">5</i>일</li>
+						</ul>
+						<b-input-group-append>
+							<b-button variant="light">단지 검침일 일괄 변경</b-button>
+						</b-input-group-append>
+					</b-input-group>
 				</b-form-group>
 			</div>
 		</div>
