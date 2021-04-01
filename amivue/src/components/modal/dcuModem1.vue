543fafa7dc61c44889a391c913c428a7f676a641
hong <tlsdoakstp@gmail.com>
2021-04-01 11:12:26 +0900

    장애-네트워크 상태 #15
    모달 추가
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

---
+++ amivue/src/components/modal/dcuModem1.vue
@@ -0,0 +1,16 @@
+<template>
+	<b-modal id="dcuModem1" title="모뎀">
+		<!---->
+		<div class="modal-content-wrap">
+			핑테스트? 재부팅... 포토샵 디자인 필요
+			<div class=""></div>
+		</div>
+		<!---->
+	</b-modal>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
