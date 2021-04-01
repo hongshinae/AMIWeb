b5b3eee2d68dbc435304dcc8e82350398e569730
hong <tlsdoakstp@gmail.com>
2021-04-02 00:09:08 +0900

    고객지원-Q&A #17
    아코디언 html 작업 테스트
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- amivue/src/pages/Menu/Support/QnA.vue
+++ amivue/src/pages/Menu/Support/QnA.vue
@@ -17,20 +17,25 @@
 			<div class="wbox">
 				<b-button v-b-toggle:my-collapse variant="none">
 					<ul>
-						<li>정기검침 또는 월검침 확인 방법</li>
-						<li>
-							<span class="when-open">Close</span>
-							<span class="when-closed">Open</span>
-						</li>
+						<li class="icon-bg"><span>Q</span></li>
+						<li class="title">정기검침 또는 월검침 확인 방법</li>
 					</ul>
 				</b-button>
 				<b-collapse id="my-collapse">
-					<div class="">A</div>
-					<p>
-						정기 검침일은 매년 1월, 6월입니다. 월검침일 매월 23일이여, 해당 지역에 따라 달라 질수 있습니다. 검침 메뉴에서 월검침을 선택하시면
-						계량기에서 정기검침일로 설정되어진 정기검침 정보와 검침일 기준으로 00시 00분 데이터를 기반으로 한 월 검침을 확인 할 수 있습니다. 검침
-						메뉴에서 월검침을 선택하시면 계량기에서 정기검침일로 설정되어진 정기검보는 웹에서 확인이 가능합니다.
-					</p>
+					<ul>
+						<li class="icon-bg">A.</li>
+						<li>
+							<div class="">
+								<p>정기 검침일은 매년 1월, 6월입니다.</p>
+								<p>월검침일 매월 23일이여, 해당 지역에 따라 달라 질수 있습니다.</p>
+								<p>
+									검침 메뉴에서 월검침을 선택하시면 계량기에서 정기검침일로 설정되어진 정기검침 정보와 검침일 기준으로 00시 00분 데이터를
+									기반으로 한 월 검침을 확인 할 수 있습니다. 검침 메뉴에서 월검침을 선택하시면 계량기에서 정기검침일로 설정되어진 정기검보는
+									웹에서 확인이 가능합니다.
+								</p>
+							</div>
+						</li>
+					</ul>
 				</b-collapse>
 			</div>
 			<div class="wbox">
