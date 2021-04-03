377bbc4e87c084c064b34da18fba4074debe25f8
hong <tlsdoakstp@gmail.com>
2021-04-04 02:12:53 +0900

    장애-네트워크 상태 #15
    - svg src 변경
    - modal header, footer 템플릿 추가
    - class="form-control" 삭제
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- amivue/src/components/modal/dcuModem1.vue
+++ amivue/src/components/modal/dcuModem1.vue
@@ -1,6 +1,27 @@
 <template>
-	<b-modal id="dcuModem1" title="모뎀">
-		<!---->
+	<b-modal id="dcuModem1">
+		<template #modal-header="{ close }">
+			<ul>
+				<li><h4>DCU MODEMV1</h4></li>
+				<li>
+					<b-button size="sm" variant="outline-light" @click="close()">
+						X
+					</b-button>
+				</li>
+			</ul>
+		</template>
+		<template #modal-footer="{ ok, cancel }">
+			<div class="btn-wrap">
+				<ul>
+					<li><!-- Button with custom close trigger value --></li>
+					<li>
+						<b-button variant="light" @click="ok()">등록</b-button>
+						<b-button variant="light" @click="cancel()">취소</b-button>
+					</li>
+				</ul>
+			</div>
+			<!-- Button with custom close trigger value -->
+		</template>
 		<div class="modal-content-wrap">
 			핑테스트? 재부팅... 포토샵 디자인 필요
 			<div class=""></div>
--- amivue/src/pages/Menu/Failure/Status.vue
+++ amivue/src/pages/Menu/Failure/Status.vue
@@ -6,7 +6,7 @@
 			<div class="main-location">
 				<b-breadcrumb>
 					<b-breadcrumb-item to="/dashboard">
-						<b-icon icon="house" scale="1.25" shift-v="1.25" aria-hidden="true"></b-icon>
+						<b-icon icon="house"></b-icon>
 						홈
 					</b-breadcrumb-item>
 					<b-breadcrumb-item>장애</b-breadcrumb-item>
@@ -14,7 +14,7 @@
 				</b-breadcrumb>
 			</div>
 		</div>
-		<div class="search-wrap1">
+		<div class="search-wrap">
 			<div class="wbox">
 				<div class="search-img">
 					<b-icon icon="search" variant="primary"></b-icon>
@@ -29,7 +29,7 @@
 										<label class="d-block">지역 이름</label>
 									</b-col>
 									<b-col lg="8">
-										<b-form-select v-model="selected" class="form-control">
+										<b-form-select v-model="selected">
 											<b-form-select-option>서울시</b-form-select-option>
 											<b-form-select-option>경기도</b-form-select-option>
 										</b-form-select>
@@ -42,7 +42,7 @@
 										<label class="d-block">단지 명</label>
 									</b-col>
 									<b-col lg="8">
-										<b-form-select v-model="selected" class="form-control">
+										<b-form-select v-model="selected">
 											<b-form-select-option>1단지</b-form-select-option>
 											<b-form-select-option>2단지</b-form-select-option>
 										</b-form-select>
@@ -55,7 +55,7 @@
 										<label class="d-block">DCU ID</label>
 									</b-col>
 									<b-col lg="8">
-										<b-form-input v-model="text" placeholder="DCU ID" class="form-control"></b-form-input>
+										<b-form-input v-model="text" placeholder="DCU ID"></b-form-input>
 									</b-col>
 								</b-row>
 							</b-col>
