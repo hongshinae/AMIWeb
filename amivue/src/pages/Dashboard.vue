e16f6d3144bb8503e5b37048c6f53b4db7e5589d
hong <tlsdoakstp@gmail.com>
2021-04-13 15:12:10 +0900

    메인페이지 구현 (공통포함) #1
    메인화면 날씨, 데이터 날씨 svg 변경 작업.
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- amivue/src/pages/Dashboard.vue
+++ amivue/src/pages/Dashboard.vue
@@ -50,13 +50,13 @@
 				<div class="row double">
 					<div class="col-lg-6">
 						<div class="todayWeather box">
-							<h4>오늘의 날씨</h4>
+							<h4>오늘의<br />날씨</h4>
 							<span class="good"></span>
 						</div>
 					</div>
 					<div class="col-lg-6">
 						<div class="detaeWeather box">
-							<h4>데이터 날씨</h4>
+							<h4>데이터<br />날씨</h4>
 							<span class="good"></span>
 						</div>
 					</div>
