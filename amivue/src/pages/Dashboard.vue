abe06e6df121b10aebb0aaac400eb1e19e00fe01
hong <tlsdoakstp@gmail.com>
2021-04-17 02:07:45 +0900

    메인페이지 구현 (공통포함) #
    대시보드 메인 화면 디자인 수정작업,
    - 메인이미지 깨짐
    - 테이블 길이조정
    - 날씨 애니메이션 효과 추가
    - 전체 패딩값 수정
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- amivue/src/pages/Dashboard.vue
+++ amivue/src/pages/Dashboard.vue
@@ -51,13 +51,13 @@
 					<div class="col-lg-6">
 						<div class="todayWeather box">
 							<h4>오늘의<br />날씨</h4>
-							<span class="good"></span>
+							<span class="good pulsate-bck"></span>
 						</div>
 					</div>
 					<div class="col-lg-6">
 						<div class="detaeWeather box">
 							<h4>데이터<br />날씨</h4>
-							<span class="good"></span>
+							<span class="good pulsate-bck"></span>
 						</div>
 					</div>
 				</div>
