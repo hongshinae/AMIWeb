754ad3bbb32425179b3e8fe7db78f8747e68b560
hong <tlsdoakstp@gmail.com>
2021-04-19 11:48:21 +0900

    설비-장비관리 #4
    장비관리 수도 svg 디자인 작업.
    기존의 전기는 주석처리함.
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- amivue/src/components/modal/detailEquipmentOther.vue
+++ amivue/src/components/modal/detailEquipmentOther.vue
@@ -30,6 +30,60 @@
 		</template>
 		<div class="svg-wrap">
 			<div class="svg">
+				<img src="@/assets/svg/waterworks.svg" alt="" title="" />
+			</div>
+			<div class="svg-input other">
+				<ul>
+					<li>
+						<h4>수도</h4>
+						<div class="meter-value">10000<span class="blink">.</span></div>
+						<b-form-group label="Meter ID" label-for="">
+							<b-form-input id="" placeholder="Meter ID"></b-form-input>
+						</b-form-group>
+						<b-form-group label="Modem MAC" label-for="">
+							<b-form-input id="" placeholder="00:00:AC:5E:A0;39:04"></b-form-input>
+						</b-form-group>
+						<b-form-group label="Device Name" label-for="">
+							<b-form-input id="" placeholder="WZT 161201a 21"></b-form-input>
+						</b-form-group>
+					</li>
+					<li>
+						<b-form-group label="Gateway ID" label-for="">
+							<b-form-input id="" placeholder="NS09_0101A"></b-form-input>
+						</b-form-group>
+						<b-form-group label="검침일" label-for="">
+							<b-input-group>
+								<b-form-input placeholder="10" disabled></b-form-input>
+								<b-input-group-append>
+									<b-button variant="light">검침일</b-button>
+								</b-input-group-append>
+							</b-input-group>
+						</b-form-group>
+						<b-form-group label="계량기 시각" label-for="">
+							<b-input-group>
+								<b-form-input placeholder="2019-05-09 11:11:00" disabled></b-form-input>
+								<b-input-group-append>
+									<b-button variant="light">시각설정</b-button>
+								</b-input-group-append>
+							</b-input-group>
+						</b-form-group>
+						<b-form-group label="LP 주기" label-for="">
+							<b-input-group>
+								<b-form-input placeholder="15" disabled></b-form-input>
+								<b-input-group-append>
+									<b-button variant="light">LP 주기</b-button>
+								</b-input-group-append>
+							</b-input-group>
+						</b-form-group>
+						<b-form-group label="계량기 타입" label-for="">
+							<b-form-input id="" placeholder="계량기 타입"></b-form-input>
+						</b-form-group>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<!--<div class="svg-wrap">
+			<div class="svg">
 				<img src="@/assets/svg/meter.svg" alt="" title="" />
 			</div>
 			<div class="svg-input">
@@ -80,7 +134,7 @@
 					</li>
 				</ul>
 			</div>
-		</div>
+		</div>-->
 	</b-modal>
 </template>
 
