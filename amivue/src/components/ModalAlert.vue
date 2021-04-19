e6118d64ae770a170a3063392411f4bf27eb3b7d
LeePyeongBok <pblee@cnuglobal.com>
2021-04-19 13:40:28 +0900

    메인페이지 구현 (공통포함) #1
    공통모듈 재탑재
    
    Signed-off-by: LeePyeongBok <pblee@cnuglobal.com>

---
+++ amivue/src/components/ModalAlert.vue
@@ -0,0 +1,42 @@
+<template>
+	<b-modal id="alert" no-close-on-backdrop auto-focus-button="ok">
+		<template #modal-header="{ close }">
+			<ul>
+				<li>
+					<h4>{{ title }}</h4>
+				</li>
+				<li>
+					<b-button size="sm" variant="outline-light" @click="close()">
+						X
+					</b-button>
+				</li>
+			</ul>
+		</template>
+		<template #modal-footer="{ ok }">
+			<div class="btn-wrap">
+				<ul>
+					<li><!-- Button with custom close trigger value --></li>
+					<li>
+						<b-button variant="light" @click="ok()" autofocus>확인</b-button>
+					</li>
+				</ul>
+			</div>
+			<!-- Button with custom close trigger value -->
+		</template>
+		<div class="modal-content-wrap">
+			<div class="modal-box">
+				{{ message }}
+			</div>
+		</div>
+		<!---->
+	</b-modal>
+</template>
+
+<script>
+export default {
+	props: { message: { type: String, default: "오류가 발생하였습니다." }, title: { type: String, default: "알 수 없는 오류" } },
+	methods: {}
+};
+</script>
+
+<style></style>
