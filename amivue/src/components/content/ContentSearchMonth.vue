10eccfb243504b17bdfb4573b0eb42361d2da0b9
hong <tlsdoakstp@gmail.com>
2021-04-29 02:16:56 +0900

    검침-정기검침 #12
     (화살표 디자인, X버튼 감추기, 전체적으로 수정)
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- amivue/src/components/content/ContentSearchMonth.vue
+++ amivue/src/components/content/ContentSearchMonth.vue
@@ -30,4 +30,21 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.vue-monthly-picker .vmp-clear-icon {
+	display: none !important;
+}
+.flexbox {
+	margin: 5px;
+	border: 1px solid #dee2e6;
+	border-radius: 4px;
+}
+.flexbox.monthItem {
+	margin: 5px;
+	border-radius: 4px;
+	border-top: none;
+}
+.picker .flexbox:first-child div {
+	padding: 7px 0 !important;
+}
+</style>
