7988484c65a72f256565cbd5dbd825744a5148f7
hong <tlsdoakstp@gmail.com>
2021-05-17 00:49:17 +0900

    퍼블리싱 컴포넌트 추가해주세요 #57
    scss 분리작업.
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- amivue/src/pages/Menu/Metering/Info/CollectionInfo.vue
+++ amivue/src/pages/Menu/Metering/Info/CollectionInfo.vue
@@ -180,4 +180,7 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scope>
+@import "~@/assets/scss/table/tableCommon.scss";
+@import "~@/assets/scss/table/tableWhite.scss";
+</style>
