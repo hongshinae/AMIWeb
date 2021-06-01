b0df94346e95f7b0b15a67cc541c3819a6533a36
hong <tlsdoakstp@gmail.com>
2021-06-02 01:49:32 +0900

    메인대시보드 전기 가스 수도 온수 난방 아이콘 추가 #67
    css 인터렉션 수정 예정.
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- amivue/src/Template/chart/DashboardType.vue
+++ amivue/src/Template/chart/DashboardType.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="presentMenu">
 		<ul>
-			<li>난방</li>
-			<li>가스</li>
-			<li>수도</li>
-			<li>온수</li>
-			<li class="on">전기</li>
+			<li class="on pulsate-fwd"><button type="button" class="btn">전기</button></li>
+			<li><button type="button" class="btn">온수</button></li>
+			<li><button type="button" class="btn">수도</button></li>
+			<li><button type="button" class="btn">가스</button></li>
+			<li><button type="button" class="btn">난방</button></li>
 		</ul>
 	</div>
 </template>
