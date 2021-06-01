d4a04f42001ef5b42c0b1d55c8382eda4927ef85
hong <tlsdoakstp@gmail.com>
2021-06-02 01:52:15 +0900

    메인대시보드 등록장비 수 수정 #69
    svg 안보이는 현상 수정예정.
    
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- amivue/src/Template/chart/AmiStatusBoard.vue
+++ amivue/src/Template/chart/AmiStatusBoard.vue
@@ -1,13 +1,60 @@
 <template>
-	<div class="box power">
+	<div class="box">
 		<h5>
 			<span>AMI 시스템 현황</span>
 			<b class="fontC">설치 수 / 장애 수</b>
 		</h5>
-		<div style="height: 130px;">
-			AMI 시스템 현황 디자인
+		<div>
+			<ul class="amiBoard">
+				<li>
+					<ul>
+						<li><img src="@/assets/svg/icon_dashboard_meter.svg" /></li>
+						<li>METER</li>
+						<li><span>145000 / </span><span class="r-color blink">1000</span></li>
+					</ul>
+				</li>
+				<li>
+					<div class="row">
+						<div class="col-sm-12 col-md-6">
+							<ul>
+								<li><img src="@/assets/svg/icon_dashboard_server.svg" /></li>
+								<li>SERVER</li>
+								<li><span>2 / </span><span class="r-color blink">1</span></li>
+							</ul>
+						</div>
+						<div class="col-sm-12 col-md-6">
+							<ul>
+								<li><img src="@/assets/svg/icon_dashboard_dcu.svg" /></li>
+								<li>DCU</li>
+								<li><span>120 / </span><span class="r-color blink">120</span></li>
+							</ul>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="row">
+						<div class="col-sm-12 col-md-6">
+							<ul>
+								<li><img src="@/assets/svg/icon_dashboard_modem.svg" /></li>
+								<li>MODEM</li>
+								<li><span>600 / </span><span class="r-color blink">50</span></li>
+							</ul>
+						</div>
+						<div class="col-sm-12 col-md-6">
+							<ul>
+								<li><img src="@/assets/svg/icon_dashboard_wifi.svg" /></li>
+								<li>LTE</li>
+								<li><span>208 / </span><span class="r-color blink">1</span></li>
+							</ul>
+						</div>
+					</div>
+				</li>
+			</ul>
 		</div>
 	</div>
 </template>
 
 <script></script>
+<style lang="scss">
+@import "~@/assets/scss/components/amiStatusBoard.scss";
+</style>
--- amivue/src/Template/chart/TodayFaultBoard.vue
+++ amivue/src/Template/chart/TodayFaultBoard.vue
@@ -1,50 +1,50 @@
 <template>
-	<div class="box power">
+	<div class="box">
 		<h5>
 			<span>상태 정보</span>
 			<b class="fontC">총 104건</b>
 		</h5>
-		<div style="height: 205px;">
-			<ul>
+		<div>
+			<ul class="faultWrap">
 				<li>
 					<p>정전</p>
-					<p>10건</p>
+					<p>0건</p>
 				</li>
 				<li>
 					<p>모뎀커버</p>
-					<p>10건</p>
+					<p class="on">10건</p>
 				</li>
 				<li>
 					<p>Sag/Swell</p>
-					<p>10건</p>
+					<p>0건</p>
 				</li>
 				<li>
 					<p>터미널커버</p>
-					<p>10건</p>
+					<p>0건</p>
 				</li>
 				<li>
 					<p>오결선</p>
-					<p>10건</p>
+					<p>0건</p>
 				</li>
 				<li>
 					<p>시각변경</p>
-					<p>10건</p>
+					<p>0건</p>
 				</li>
 				<li>
 					<p>온도</p>
-					<p>10건</p>
+					<p class="on">12건</p>
 				</li>
 				<li>
 					<p>프로그램변경</p>
-					<p>10건</p>
+					<p>0건</p>
 				</li>
 				<li>
 					<p>배터리 없음</p>
-					<p>10건</p>
+					<p class="on">99건</p>
 				</li>
 				<li>
 					<p>그 외</p>
-					<p>10건</p>
+					<p>0건</p>
 				</li>
 			</ul>
 		</div>
@@ -52,3 +52,6 @@
 </template>
 
 <script></script>
+<style lang="scss">
+@import "~@/assets/scss/components/todayFaultBoard.scss";
+</style>
